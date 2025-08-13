f14d6f920c5caf17353f5480a0a57c9f1b6cf19f
mj_mohona <mj.mohonajahan@gmail.com>
2025-08-13 23:42:45 +0600

    first commit

--- test.js
+++ test.js
@@ -90,7 +90,8 @@
 // console.log(numbers);
 
 //map method----->
-const numbers = [1, 2, 3, 4, 5, 6];
-const map = numbers.map((num) => num * 2);
-console.log(map);
-console.log(numbers);
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const map = numbers.map((num) => num * 2);
+// console.log(map);
+// console.log(numbers);
+
